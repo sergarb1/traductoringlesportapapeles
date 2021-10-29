e0b339e01877e8621c096fd3f62c0528ec878a76
sergarb1 <sergi.profesor@gmail.com>
2021-10-29 14:25:26 +0200

    Primera actualizacion

---
+++ traducirClipESP2ENG.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+'''
+Para la traduccion
+pip3 install deep-translator
+
+Documentación biblioteca
+pip3 install pyperclip
+https://pyperclip.readthedocs.io/en/latest/
+Si da "Not implemented Error"
+sudo apt-get install curl #to install curl
+sudo apt-get install xsel #to install the xsel utility.
+sudo apt-get install xclip #to install the xclip utility.
+pip install gtk #to install the gtk Python module.
+pip install PyQt4 #to install the PyQt4 Python modu
+
+'''
+
+
+from deep_translator import GoogleTranslator
+
+import pyperclip as pc
+
+import time
+
+
+while True:
+    tmp_value= pc.waitForNewPaste()
+    time.sleep(0.1)
+    #Obtenemos datos del portapapeles
+    datos = pc.paste().strip()
+    #Impresion de depuracion print(datos)
+    #Traducimos
+    traduccion = GoogleTranslator(source='es', target='en').translate(datos)  # output -> Weiter so, du bist großarti
+    #Impresion de depuracion print(traduccion)
+    pc.copy(traduccion)
